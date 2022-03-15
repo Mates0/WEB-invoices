ddbd88bbc4b69b66932a760255df16bf6ca22024
Mates0 <matyas.vanko@educanet.cz>
2022-03-15 13:53:28 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+window.onload = () => {
+    document.getElementById("addBtn").addEventListener("click", () => {
+        console.log("test")
+        fetch("http://164.92.142.211/87ec1114-49ae-4e04-9774-909f851ba57a/Invoices", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                to: document.getElementById("to").value,
+                amount: document.getElementById("amount").value
+            })
+        }).then(res => {
+            window.location.href = 'index.html'
+        })
+    })
+}
